75605c56e0c174e45791457c9e7b06d1a7c77c10
tsubasa ooishi <pop331240283@icloud.com>
2018-07-15 13:08:57 +0900

    Finish 4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'new'
+    end
+  end
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'edit'
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    # binding.pry
+    book.destroy
+    redirect_to new_book_path(@book)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
